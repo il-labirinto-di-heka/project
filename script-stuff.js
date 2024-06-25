7a5800bcb9750d66e911cf09963fa665f5577df4
il-labirinto-di-heka <neologismi.dndstuff@gmail.com>
2024-06-25 06:32:43 +0200

    Update script-stuff.js

--- script-stuff.js
+++ script-stuff.js
@@ -2,17 +2,13 @@
 // STICKY MENU
 // -----------
 
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {stickyMenu()};
 
-// Get the header
 var menu = document.querySelector(".menu");
 var main = document.querySelector(".main");
 
-// Get the offset position of the navbar
 var sticky = menu.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyMenu() {
   if (window.scrollY > sticky) {
     menu.classList.add("sticky");
